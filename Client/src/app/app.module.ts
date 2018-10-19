c8aa99c379b183c4e6858a70ff1321cf1be17125
Dan <danpopa1998@gmail.com>
2018-10-20 02:06:00 +0300

    add post component

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
---
+++ Client/src/app/post.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class PostComponent {
+  text = 'Hey';
+}
