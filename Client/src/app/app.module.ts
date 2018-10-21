1f3265edb10362a5e79fe58bdcae24832ff58bd1
Dan <danpopa1998@gmail.com>
2018-10-21 16:56:11 +0300

    start adding router

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post.component';
+import { AdminListComponent } from './pages/admin-list/admin-list.component';
+import { AdminAddComponent } from './pages/admin-add/admin-add.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'admin/add', component: AdminAddComponent },
+  { path: 'admin', component: AdminListComponent },
+];
 
 @NgModule({
   declarations: [
+    AdminAddComponent,
+    AdminListComponent,
     AppComponent,
     PostComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
