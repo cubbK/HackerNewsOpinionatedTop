a869dc40a77aa383e84411d06e2fdf231043e16d
Dan <danpopa1998@gmail.com>
2018-11-04 23:51:21 +0200

    add routing

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -6,21 +6,27 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
 
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from './material.module';
 
-import { PostService } from './services/post.service'
+import { PostService } from './services/post.service';
 
 const appRoutes: Routes = [
-  // { path: 'admin', component: AdminAddComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
   // { path: 'admin', component: AdminListComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
